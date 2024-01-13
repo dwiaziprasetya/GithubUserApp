58f13018a1018085dc7e340506d0d4606f3fd157
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2024-01-14 03:27:47 +0700

    Menambahkan fitur search bar

--- app/src/main/java/com/example/githubuserapp/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/githubuserapp/ui/search/SearchFragment.kt
@@ -1,10 +1,13 @@
 package com.example.githubuserapp.ui.search
 
 import android.os.Bundle
+import android.text.Html
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.githubuserapp.R
 import com.example.githubuserapp.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
@@ -20,4 +23,19 @@ class SearchFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding){
+            searchBar.inflateMenu(R.menu.search_bar_menu)
+            searchView.setupWithSearchBar(searchBar)
+            searchView
+                .editText
+                .setOnEditorActionListener { textView, actionId, event ->
+                    searchBar.setText(searchView.text)
+                    searchView.hide()
+                    Toast.makeText(requireActivity(), searchView.text, Toast.LENGTH_SHORT).show()
+                    false
+                }
+        }
+    }
 }
