94721495911271b4e2a4348ee78856537a41c045
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2024-01-14 22:42:13 +0700

    Menambahkan dependency cardview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
--- app/src/main/java/com/example/githubuserapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/githubuserapp/ui/activity/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.githubuserapp
+package com.example.githubuserapp.ui.activity
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.githubuserapp.R
 import com.example.githubuserapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
