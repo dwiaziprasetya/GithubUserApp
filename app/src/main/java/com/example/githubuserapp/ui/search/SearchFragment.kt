59dba60cc5a4c4dca7f3aef81296c5d572ddd1e9
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2024-01-19 03:51:29 +0700

    Menyelesaikan semua fitur 1 fragment search (belum dirapikan)

--- app/src/main/java/com/example/githubuserapp/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/githubuserapp/ui/search/SearchFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.githubuserapp.R
 import com.example.githubuserapp.adapter.SearchAdapter
@@ -35,65 +37,64 @@ class SearchFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val mainViewModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory())[SearchViewModel::class.java]
+
+
+        mainViewModel.dataPerson.observe(viewLifecycleOwner){
+            setData(it)
+        }
+
+        mainViewModel.isLoading.observe(viewLifecycleOwner) {
+            showLoading(it)
+        }
+
+        mainViewModel.isSearching.observe(viewLifecycleOwner) {
+            showNestedScrollView(it)
+        }
+
+        // Search Bar / Search View Action
         with(binding){
             searchBar.inflateMenu(R.menu.search_bar_menu)
             searchView
                 .editText
                 .setOnEditorActionListener { textView, actionId, event ->
                     searchBar.setText(searchView.text)
-                    binding.incLayoutSearchPerson.constraint.visibility = View.GONE
-                    setDataPerson(textView.text.toString())
+                    mainViewModel.setDataPerson(textView.text.toString())
+                    mainViewModel.setSearchingState(true)
                     searchView.hide()
                     false
                 }
         }
 
-
+        // RecyclerView
         binding.rvPersonList.hasFixedSize()
         binding.rvPersonList.layoutManager = LinearLayoutManager(requireActivity())
-
-//        binding.searchPersonTemplate.constraint.visibility = View.INVISIBLE
     }
 
-    private fun setDataPerson(query : String) {
-        binding.nsvContent.visibility = View.VISIBLE
-        val client = ApiConfig.getApiService().getSearch(query)
-        client.enqueue(object : Callback<SearchResponse> {
-            override fun onResponse(
-                call: Call<SearchResponse>,
-                response: Response<SearchResponse>
-            ) {
-                showLoading(false)
-                if (response.isSuccessful) {
-                    val responseBody = response.body()
-                    if (responseBody != null) {
-                        list.clear()
-                        setData(responseBody.items)
-                        val adapter = SearchAdapter(list)
-                        binding.rvPersonList.adapter = adapter
-                    }
-                }
-            }
-
-            override fun onFailure(call: Call<SearchResponse>, t: Throwable) {
-                showLoading(false)
-            }
-
-        })
-    }
 
     private fun setData(person : List<ItemsItem>) {
+        list.clear()
         list.addAll(person)
+        val adapter = SearchAdapter(list)
+        binding.rvPersonList.adapter = adapter
         for (item in list) {
             Log.d("API_RESPONSE", "Item Name: ${item.login}, Item Name: ${item.id}")
         }
     }
 
     private fun showLoading(isLoading : Boolean) {
-        if (isLoading) {
-            binding.pbSearchPerson.visibility = View.VISIBLE
+        Log.d("SearchFragment", "showLoading: $isLoading")
+        binding.pbSearchPerson.visibility = if (isLoading) View.VISIBLE else View.GONE
+    }
+
+    private fun showNestedScrollView(isSearching: Boolean) {
+        if (isSearching){
+            binding.nsvContent.visibility = View.VISIBLE
+            binding.incLayoutSearchPerson.constraint.visibility = View.GONE
         } else {
-            binding.pbSearchPerson.visibility = View.GONE
+            binding.nsvContent.visibility = View.INVISIBLE
+            binding.incLayoutSearchPerson.constraint.visibility = View.VISIBLE
         }
     }
 }
--- app/src/main/java/com/example/githubuserapp/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/example/githubuserapp/ui/search/SearchViewModel.kt
@@ -3,6 +3,46 @@ package com.example.githubuserapp.ui.search
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.githubuserapp.response.ItemsItem
+import com.example.githubuserapp.response.SearchResponse
+import com.example.githubuserapp.retrofit.ApiConfig
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SearchViewModel : ViewModel() {
+    private val _dataPerson = MutableLiveData<List<ItemsItem>>()
+    val dataPerson: LiveData<List<ItemsItem>> = _dataPerson
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _isSearching = MutableLiveData<Boolean>()
+    val isSearching: LiveData<Boolean> = _isSearching
+
+
+    fun setSearchingState(isSearching: Boolean) {
+        _isSearching.value = isSearching
+    }
+
+    fun setDataPerson(query : String) {
+        _isLoading.value = true
+        val client = ApiConfig.getApiService().getSearch(query)
+        client.enqueue(object : Callback<SearchResponse> {
+            override fun onResponse(
+                call: Call<SearchResponse>,
+                response: Response<SearchResponse>
+            ) {
+                _isLoading.value = false
+                if (response.isSuccessful) {
+                    _dataPerson.value = response.body()?.items
+                }
+            }
+
+            override fun onFailure(call: Call<SearchResponse>, t: Throwable) {
+                _isLoading.value = false
+            }
+
+        })
+    }
 }
