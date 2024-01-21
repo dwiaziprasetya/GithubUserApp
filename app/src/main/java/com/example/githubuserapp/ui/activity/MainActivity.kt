76902233b8fd920f8a6de8f7722d7f70af137dac
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2024-01-21 07:02:06 +0700

    Menambahkan animasi pada navigation search fragment to detail fragment

--- app/src/main/java/com/example/githubuserapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/githubuserapp/ui/activity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
+import androidx.navigation.ActivityNavigator
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
