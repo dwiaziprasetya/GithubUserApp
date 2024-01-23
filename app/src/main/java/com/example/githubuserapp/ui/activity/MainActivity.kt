a73ec664e21fa0004596fa4dfe9332bfdf7c37af
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2024-01-24 02:53:35 +0700

    Menyelesaikan dan berhasil menampilkan data followers (belum dirapikan)

--- app/src/main/java/com/example/githubuserapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/githubuserapp/ui/activity/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.example.githubuserapp.ui.activity
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentManager
-import androidx.navigation.ActivityNavigator
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.githubuserapp.R
 import com.example.githubuserapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,7 +14,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
