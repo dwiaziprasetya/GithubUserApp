01d0f9c5b062817582e35c2cfa1941a81e881ab8
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2024-01-24 11:33:48 +0700

    Menambahkan badge icon di menu icon home fragment

--- app/src/main/java/com/example/githubuserapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/githubuserapp/ui/home/HomeFragment.kt
@@ -1,11 +1,16 @@
 package com.example.githubuserapp.ui.home
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.githubuserapp.R
 import com.example.githubuserapp.databinding.FragmentHomeBinding
+import com.google.android.material.badge.BadgeDrawable
+import com.google.android.material.badge.BadgeUtils
 
 class HomeFragment : Fragment() {
 
@@ -20,6 +25,29 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    @SuppressLint("UnsafeOptInUsageError")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Icon Badge
+        val backgroundColor = ContextCompat.getColor(requireContext(), R.color.red)
+        val toolbar = binding.toolbar
+        val badgeDrawableMessage = BadgeDrawable.create(requireActivity()).apply {
+            isVisible = true
+            this.backgroundColor = backgroundColor
+            number = 5
+        }
+        val badgeDrawableNotification = BadgeDrawable.create(requireActivity()).apply {
+            isVisible = true
+            this.backgroundColor = backgroundColor
+            number = 5
+        }
+        BadgeUtils.attachBadgeDrawable(badgeDrawableMessage, toolbar, R.id.message_menu)
+        BadgeUtils.attachBadgeDrawable(badgeDrawableNotification, toolbar, R.id.notification_menu)
+    }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
