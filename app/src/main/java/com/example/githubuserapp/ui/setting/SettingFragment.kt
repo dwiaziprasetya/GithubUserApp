f44f10fa1e17f9049b01f4caa94479c014fb26d4
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2024-02-04 19:03:31 +0700

    Membuat fragment settings

---
+++ app/src/main/java/com/example/githubuserapp/ui/setting/SettingFragment.kt
@@ -0,0 +1,17 @@
+package com.example.githubuserapp.ui.setting
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.githubuserapp.R
+
+class SettingFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_setting, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/githubuserapp/ui/setting/SettingViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.githubuserapp.ui.setting
+
+import androidx.lifecycle.ViewModel
+
+class SettingViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
