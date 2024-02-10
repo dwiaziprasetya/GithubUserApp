63b8f04a4e26ed9a59dabdaefda0b499681fe1eb
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2024-02-10 21:25:44 +0700

    Menambahkan dependency mockito

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
+    testImplementation("org.mockito:mockito-core:4.4.0")
+    testImplementation("org.mockito:mockito-inline:4.4.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
